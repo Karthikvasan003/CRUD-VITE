ad8c09ac5aef8cf7d5553c7dff1d434af813e37c
Karthikvasan003 <155101031+Karthikvasan003@users.noreply.github.com>
2025-05-02 14:17:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,67 +8,44 @@ import UpdateStu from './components/UpdateStu'
 import { Toaster } from 'react-hot-toast'
 
 
-// let router = createBrowserRouter(
-//     [
-//         {
-//             path: '/',
-//             element: <Layout/>,
-//             children: [
-//                 {
-//                     index:true,
-//                     element: <CreateStudent/>
-//                 },
-//                 {
-//                     path: "/viewall",
-//                     element: <ViewAll/>
-//                 },
-//                 {
-//                     path: "/edit/:id",
-//                     element: <UpdateStu/>
-//                 },
-//                 {
-//                     path:"*",
-//                     element: <PageNotFound/>
-//                 }
-//             ]
-//         }
-//     ]
-// )
+let router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Layout/>,
+            children: [
+                {
+                    index:true,
+                    element: <CreateStudent/>
+                },
+                {
+                    path: "/viewall",
+                    element: <ViewAll/>
+                },
+                {
+                    path: "/edit/:id",
+                    element: <UpdateStu/>
+                },
+                {
+                    path:"*",
+                    element: <PageNotFound/>
+                }
+            ]
+        }
+    ]
+)
 
-// //(alias) function createBrowserRouter(routes: RouteObject[], opts?: DOMRouterOpts): Router import createBrowserRouter
-
-// const App = () => {
-//   return (
-//     <>
-//         <>
-//         <div><Toaster/></div>
-//         <RouterProvider router={router} />
-//         </>
-//     </>
-//   )
-// }
-
-// export default App
-
-let router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout/>,
-        children: [
-            {
-                index: true,
-                element: <CreateStudent/>
-            },
-            {
-                path: "/viewAll",
-                element: <ViewAll/>
-            }
-        ]
-    }
-])
+//(alias) function createBrowserRouter(routes: RouteObject[], opts?: DOMRouterOpts): Router import createBrowserRouter
 
 const App = () => {
-
+  return (
+    <>
+        <>
+        <div><Toaster/></div>
+        <RouterProvider router={router} />
+        </>
+    </>
+  )
 }
 
 export default App
